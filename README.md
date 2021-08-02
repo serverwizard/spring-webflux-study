0e821cedbc813b6360a5bbff3f9ff70c4969da29
serverwizard <serverwizard@naver.com>
2021-08-03 01:26:52 +0900

    [Spring WebFlux] mergeSequential, doOnSubscribe, doOnRequest 학습

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Day2. StepVerifier 학습
 Day3. Transform(map, flatMap) 학습
 
 Day4. flatMap, merge, concat 학습
+
+Day5. mergeSequential, doOnSubscribe, doOnRequest 학습
--- src/test/java/org/serverwizard/reactor/FlatMapTest.java
+++ src/test/java/org/serverwizard/reactor/FlatMapTest.java
@@ -6,6 +6,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -48,6 +49,32 @@ public class FlatMapTest {
     }
 
     @Test
+    void mergeTest2() {
+        Flux<Long> inFlux1 = Flux.interval(Duration.from(Duration.ofMillis(1000))).take(4);
+        Flux<Long> inFlux2 = Flux.just(1L, 2L, 3L, 4L, 5L);
+
+        Flux<Long> outFlux = Flux.merge(inFlux1, inFlux2).log();
+
+        StepVerifier.create(outFlux)
+                .thenRequest(1)
+                .expectNext(1L)
+                .expectNextCount(8)
+                .verifyComplete();
+    }
+
+    @Test
+    void mergeSequentialTest() {
+        Flux<Long> inFlux1 = Flux.interval(Duration.from(Duration.ofMillis(1000))).take(4);
+        Flux<Long> inFlux2 = Flux.just(100L, 101L, 102L, 103L);
+
+        Flux<Long> outFlux = Flux.mergeSequential(inFlux1, inFlux2).log();
+
+        StepVerifier.create(outFlux)
+                .expectNextCount(8)
+                .verifyComplete();
+    }
+
+    @Test
     void concatTest() {
         Mono<String> inMono1 = Mono.just("serverwizard");
         Mono<String> inMono2 = Mono.just("hongjongwan");
@@ -58,5 +85,21 @@ public class FlatMapTest {
                 .expectNext("serverwizard")
                 .expectNext("hongjongwan")
                 .verifyComplete();
+    }
+
+    @Test
+    void subscribeTest() {
+        Flux<String> source = Flux.just("serverwizard", "hongjongwan").log();
+
+        source.subscribe(System.out::println);
+    }
+
+    @Test
+    void doOnSubscribeTest() {
+        Flux<Integer> source = Flux.range(1, 3).log()
+                .doOnSubscribe(s -> System.out.println("do on subscribe"))
+                .doOnRequest(s -> System.out.println("do on request"));
+
+        source.subscribe(System.out::println);
     }
 }
